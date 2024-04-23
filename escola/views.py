715a9d7215fa2230419da0055d0732cee130c76e
VictorSoaresFerreira <129811759+victorsoaresferreiraa@users.noreply.github.com>
2024-04-23 17:58:15 -0300

    fazendo modificações

--- escola/views.py
+++ escola/views.py
@@ -1,8 +1,23 @@
-from django.http import JsonResponse
+"""from django.http import JsonResponse
 
 # Create your views here.
 
 def aluno(request):
     if request.method == 'GET':
         aluno = {'id':1,  'nome': 'Guilerme'}
-        return JsonResponse(aluno)
+        return JsonResponse(aluno)"""
+
+from rest_framework import viewsets
+from escola.models import Aluno, Curso
+from serializer import AlunoSerializer, CursoSerializer
+
+class AlunosViewSet(viewsets.ModelViewSet):
+    """Exibindo todos os alunos e alunas"""
+    queryset = Aluno.objects.all()
+    serializer_class = AlunoSerializer
+
+
+class CursosViewSet(viewsets.ModelViewSet):
+    """Exibindo todos os cursos"""
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializer
